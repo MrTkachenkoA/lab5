19ba1129d20c1716f18e920a4e550a5d7540c412
MikoreVikt <136119613+MrTkachenkoA@users.noreply.github.com>
2023-06-11 20:41:03 +0300

    adds start file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,43 @@
-import { TouristRoute1 } from "./TouristRoute1";
-import { TouristRoute2 } from "./TouristRoute2";
+import { useState } from "react";
+import "../index.css";
+import { TouristRoutes } from "./TouristRoutes";
 
 export const App = () => {
-  const route1 = {
-    name: "Вишенське озеро",
-    length: "12km",
-    difficulty: "6/10",
-    time: "3h",
-  };
-  const route2 = {
-    name: "Черепашинецький кар'єр",
-    length: "21km",
-    difficulty: "9/10",
-    time: "6h",
+  const [routes, setRoutes] = useState([
+    {
+      id: 1,
+      name: "Вишенське озеро",
+      length: "12km",
+      difficulty: "6/10",
+      time: "3h",
+    },
+    {
+      id: 2,
+      name: "Черепашинецький кар'єр",
+      length: "21km",
+      difficulty: "9/10",
+      time: "6h",
+    },
+  ]);
+
+  console.log(routes);
+
+  const handleDeleteRoute = (id) => {
+    setRoutes(routes.filter((route) => route.id !== id));
   };
 
   return (
-    <div>
-      {/* Спосіб 1: Передача параметрів через пряме використання */}
-      <TouristRoute1
-        name={route1.name}
-        length={route1.length}
-        difficulty={route1.difficulty}
-        time={route1.time}
-      />
-
-      {/* Спосіб 2: Передача параметрів через об'єкт */}
-      <TouristRoute2 route={route2} />
+    <div className="tourist-routes_wrapper">
+      {routes.map(({ id, name, length, difficulty, time }) => (
+        <TouristRoutes
+          key={id}
+          name={name}
+          length={length}
+          difficulty={difficulty}
+          time={time}
+          onDeleteRoute={() => handleDeleteRoute(id)}
+        />
+      ))}
     </div>
   );
 };
